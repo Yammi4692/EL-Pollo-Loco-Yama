2ce4b05a4f13de9254bef79ce2a812692f2e3b82
Yammi4692 <aziziyama92@gmail.com>
2026-02-09 21:36:27 +0100

    draw player

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 ﻿let canvas;
 let ctx;
 let bg;
+let player;
 
 function init() {
   canvas = document.getElementById('game');
   ctx = canvas.getContext('2d');
 
   bg = new Background('img/img/5_background/first_half_background.png');
+  player = new Player(
+    'img/img/2_character_pepe/1_idle/idle/I-1.png',
+    40, 260, 120, 160
+  );
 
   bg.img.onload = () => {
     draw();
@@ -15,6 +20,7 @@ function init() {
 
 function draw() {
   bg.draw(ctx, canvas);
+  player.draw(ctx);
 }
 
 init();
