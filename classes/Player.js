7509fac9b8da3028bc88cdf56456df9ca256a6f2
Yammi4692 <aziziyama92@gmail.com>
2026-02-09 22:22:56 +0100

    fix idle frame

--- classes/Player.js
+++ classes/Player.js
@@ -2,15 +2,34 @@ class Player {
   constructor(src, x, y, w, h) {
     this.img = new Image();
     this.img.src = src;
+    this.idleImg = this.img;
     this.x = x;
     this.y = y;
     this.w = w;
     this.h = h;
     this.speed = 3;
+
+    this.walkFrames = [];
+    this.walkImages = [];
+    this.walkIndex = 0;
+    this.walkTimer = 0;
+    this.isWalking = false;
+  }
+
+  setWalkFrames(frames) {
+    this.walkFrames = frames;
+    this.walkImages = [];
+    frames.forEach((src) => {
+      let img = new Image();
+      img.src = src;
+      this.walkImages.push(img);
+    });
   }
 
   moveRight() {
     this.x += this.speed;
+    this.isWalking = true;
+    this.animateWalk();
   }
 
   draw(ctx) {
