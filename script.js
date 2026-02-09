c4646f2fdddb04a9df28604e05e08ecb9ba68689
Yammi4692 <aziziyama92@gmail.com>
2026-02-09 21:29:37 +0100

    draw background

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-﻿
+﻿let canvas;
+let ctx;
+let bg;
+
+function init() {
+  canvas = document.getElementById('game');
+  ctx = canvas.getContext('2d');
+
+  bg = new Background('img/img/5_background/first_half_background.png');
+
+  bg.img.onload = () => {
+    draw();
+  };
+}
+
+function draw() {
+  bg.draw(ctx, canvas);
+}
+
+init();
