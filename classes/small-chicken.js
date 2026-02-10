7265953a68307af5595e6c92df0290c81a1ec62c
Yammi4692 <aziziyama92@gmail.com>
2026-02-10 15:46:46 +0100

    add small chicken walk frames

--- classes/small-chicken.js
+++ classes/small-chicken.js
@@ -9,6 +9,19 @@ class SmallChicken extends MovableObject {
   constructor(x, y) {
     super('img/img/3_enemies_chicken/chicken_small/1_walk/1_w.png', x, y, 60, 60);
     this.speed = 0.6;
+    this.walkFrames = [
+      'img/img/3_enemies_chicken/chicken_small/1_walk/1_w.png',
+      'img/img/3_enemies_chicken/chicken_small/1_walk/2_w.png',
+      'img/img/3_enemies_chicken/chicken_small/1_walk/3_w.png'
+    ];
+    this.walkImages = [];
+    this.walkIndex = 0;
+    this.walkTimer = 0;
+    this.walkFrames.forEach((src) => {
+      let img = new Image();
+      img.src = src;
+      this.walkImages.push(img);
+    });
   }
 
   /**
@@ -16,5 +29,19 @@ class SmallChicken extends MovableObject {
    */
   moveLeft() {
     this.x -= this.speed;
+    this.animateWalk();
+  }
+
+  /**
+   * small walk animation
+   */
+  animateWalk() {
+    if (this.walkImages.length === 0) return;
+    this.walkTimer++;
+    if (this.walkTimer > 8) {
+      this.walkTimer = 0;
+      this.walkIndex = (this.walkIndex + 1) % this.walkImages.length;
+      this.img = this.walkImages[this.walkIndex];
+    }
   }
 }
